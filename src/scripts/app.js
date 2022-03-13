4ab789e0119419fd91cf3c1db4bbe3318eaec2bf
Kamil <kam.nas21@wp.pl>
2022-03-13 13:09:53 +0100

    HTML socials structure, js coding classList toggle

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,11 @@
 const shareButton = document.querySelector('[data-share]');
+const socials = document.querySelector('[data-socials]');
+const socialItems = document.querySelector('[data-items]');
 
 const toggleShareOptions = () => {
-	console.log('Hello World!');
+	socials.classList.toggle('active');
+	shareButton.classList.toggle('filter');
+	socialItems.classList.toggle('show');
 };
 
 shareButton.addEventListener('click', toggleShareOptions);
